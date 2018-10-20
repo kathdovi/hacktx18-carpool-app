6c355c7e0fc79726067ae58eafaa38b19d8481ff
Joshua Johnson <joshuaj1397@gmail.com>
2018-10-20 16:39:21 -0500

    Model abstraction

--- create-ride/api/model/rideModel.js
+++ create-ride/api/model/rideModel.js
@@ -1,12 +1,17 @@
 "use strict"
+import { User } from "./userModel.js"
+
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const RideSchema = new Schema({
+const Ride = {
   id: String,
   name: String,
   passengers: [User],
   driver: User
-})
+}
 
+const RideSchema = new Schema(Ride)
+
+export { Ride }
 module.exports = mongoose.model("Users", UserSchema)
--- create-ride/api/model/userModel.js
+++ create-ride/api/model/userModel.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema({
+const User = {
   id: String
   name: String,
   location: {
     longitude: Number,
     latitude: Number
-  },
-})
+  }
+}
 
+const UserSchema = new Schema(User)
+
+export { User }
 module.exports = mongoose.model("Users", UserSchema)
