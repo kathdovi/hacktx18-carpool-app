1b00b72f922ceebe5fa15192cd71e3b2ee1dc3b6
Benson Chu <bensonchu457@gmail.com>
2018-10-21 06:03:24 -0500

    Attempted to connect to db

--- MiddleTier/src/main.go
+++ MiddleTier/src/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"github.com/mongodb/mongo-go-driver/mongo"
+	//"github.com/mongodb/mongo-go-driver/mongo"
+	"github.com/zebresel-com/mongodm"
 	"handlers"
 	"net/http"
 )
 
+type User struct {
+	mongodm.DocumentBase `json:",inline"      bson:",inline"`
+	FirstName            string `json:"firstname"    bson:"firstname"`
+	LastName             string `json:"lastname"	 bson:"lastname"`
+	UserName             string `json:"username"	 bson:"username"`
+	//Messages  interface{} `json:"messages"	 bson:"messages" 	model:"Message" relation:"1n" autosave:"true"`
+}
+
 func main() {
 	//http.HandleFunc("/api/org", orgHandler)
-	mongo.NewClient("mongodb://bensonchu457:carpool@carpool-shard-00-00-hogxs.mongodb.net:27017," +
-		"carpool-shard-00-01-hogxs.mongodb.net:27017," +
-		"carpool-shard-00-02-hogxs.mongodb.net:27017/test?ssl=true&replicaSet=carpool-shard-0&authSource=admin&retryWrites=true")
+	// mongo.NewClient("mongodb://bensonchu457:carpool@carpool-shard-00-00-hogxs.mongodb.net:27017," +
+	// 	"carpool-shard-00-01-hogxs.mongodb.net:27017," +
+	// 	"carpool-shard-00-02-hogxs.mongodb.net:27017/test?ssl=true&replicaSet=carpool-shard-0&authSource=admin&retryWrites=true")
+	dbConfig := &mongodm.Config{
+		DatabaseHosts:    []string{"carpool-shard-00-00-hogxs.mongodb.net:27017,carpool-shard-00-01-hogxs.mongodb.net:27017,carpool-shard-00-02-hogxs.mongodb.net:27017/test?ssl=true&replicaSet=carpool-shard-0&authSource=admin&retryWrites=true"},
+		DatabaseName:     "database",
+		DatabaseUser:     "bensonchu457",
+		DatabasePassword: "carpool",
+		DatabaseSource:   "admin",
+	}
+
+	connection, _ := mongodm.Connect(dbConfig)
+	connection.Register(&User{}, "users")
 
 	http.HandleFunc("/api/trip", handlers.TripHandler)
 	http.HandleFunc("/api/response", handlers.ResponseHandler)
